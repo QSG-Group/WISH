0893a250a1858222801bc23a6e72e9bef3623677
Victor Carmelo <victor.ao.carmelo@gmail.com>
2016-06-06 11:14:23 +0200

    Complete overhaul of functions
    Generate.genotype ( previously data.import):
    data.selection changed to generate.genotype. Complete re-write of function as previous version was not working properly.
    Generate.genotype does not need gwas pvalues to filters, as it will use the given gwas_id to select snps in case of missing p-values.
    
    epistatic.correlation:
    Most input parameters removed from epistatic.correlation to remove redundancy as the selection of snps should be done in the generate.genotype function. Old linear modelling funciton Asreml replaced with fastLM from Rccp package, with great increase in speed.

--- R/all_functions.R
+++ R/all_functions.R
@@ -6,7 +6,7 @@
 #' Import genotype data in the correct format for network construction
 #' @export
 #' @description For network construction based on both
-#' @usage data.import(ped, tped, gwas_id, gwas_p, pvalue, id.select)
+#' @usage generate.genotype(ped, tped, gwas_id, gwas_p, pvalue, id.select)
 #' genomic correlations aswell epistatic interactions a genotype matrix has to be
 #' created, consisting of one numeric value per SNP, per individual. This function
 #' takes Plink output (1,2-coding) to create the genotype matrix which can be used
@@ -16,7 +16,7 @@
 #' Family ID, Idividual ID, Paternal ID, Maternal ID, 
 #' Sex (1=male; 2=female;other=unknown) and Phenotype. The IDs are alphanumeric: 
 #' the combination of family and individual ID should uniquely identify a person.
-#' A PED file must have1 and only 1 phenotype in the sixth column.
+#' A PED file must have 1 and only 1 phenotype in the sixth column.
 #' The phenotype can be either a quantitative trait or an affection status 
 #' column: PLINK will automatically detect which type
 #' (i.e. based on whether a value other than 0, 1, 2 or the missing genotype 
@@ -28,10 +28,12 @@
 #' Then all genotypes are listed for all individuals for each particular SNP on 
 #' each line. Again, SNPs are 1,2-coded.
 #' @param gwas_id  A vector of all SNPs in the GWAS
-#' @param gwas_p A vector of the p-values corresponding to the gwas_id vector
+#' @param gwas_p **optional** A vector of the p-values corresponding to 
+#' the gwas_id vector. If assigned, will select snps based on the pvalue
+#' parameter with a default value of 0.05.
 #' @param pvalue A value for the cutoff of the SNPs which should be remained 
 #' in the matrix, based on the pvalue resulting from the GWAS
-#' @param id.select If requested, asubset of individuals can be 
+#' @param id.select If requested, a subset of individuals can be 
 #' selected (e.g. extremes). If nothing inserted, all individuals are in the
 #' output
 #' @return A genotype dataframe with a row for each individual and a column
@@ -48,53 +50,52 @@
 #' data.import(ped, tped, gwas_id, gwas_p, pvalue, id.select)
 #' 
 #'
-data.import <- function(ped, tped,gwas_id,gwas_p,pvalue=0.05,id.select=ped[,2]){
-  a.s <- ped[,c(2,7:ncol(ped))]
-  snp <- rep((as.character(tped[,2])),each=2)
-  snp <- append(snp, "ID", after=0)
-  colnames(a.s) <- c(snp)
-  gwas.select <- cbind(gwas_id,gwas_p)
-  gwas.select <- as.data.frame(gwas.select)
-  gwas.select$gwas_p <- as.numeric(as.character(gwas.select$gwas_p))
-  p.select <- gwas.select[gwas.select$gwas_p < pvalue,]
-  snp.select <- p.select$gwas_id
-  snps<- rep(snp.select, each=2)
-  snps <- append(as.character(snps), "ID", after=0)
-  a.snps <- a.s[,snps]
-  rownames(a.snps) <- a.snps[,1]
-  input <- a.snps[,2:ncol(a.snps)]
-  ind <- data.frame(matrix(c((1:ncol(input)),rep(NA, 2-ncol(input)%%2)),byrow=F,nrow=2))
-  nonna <- ind[,sapply(ind, function(x) all(!is.na(x)))]
-  genotype <- do.call(cbind,lapply(nonna,function(i)rowMeans(input[,i])))
-  x <- colnames(input[1:(ncol(input)-1)])
-  colnames(genotype) <- x[c(T,F)]
-  genotype <- replace(genotype, genotype==0,"NA")
-  return(as.data.frame(genotype))
+generate.genotype <- function(ped,tped,gwas_id,pvalue=0.05,id.select=ped[,2],gwas_p=NULL) {
+  if(is.null(gwas_p)){
+    genotype <- matrix(nrow=nrow(ped),ncol=length(c(gwas_id)))
+    rownames(genotype) <- id.select
+    colnames(genotype) <- gwas_id
+    snps<-which(tped[,2]%in%gwas_id)
+    ids<-which(ped[,2]%in%id.select)
+    tped_trim <- tped[snps,c(1:4,sort(rep(2*ids,each=2)-(1:(2*length(ids)))%%2)+4)]
+    ped_trim <- ped[ids,c(1:6,sort(rep(2*snps,each=2)-(1:(2*length(snps)))%%2)+6)]
+    for (i in 1:(dim(genotype)[2])){
+      genotype[,i] <- rowMeans((ped_trim[,c(2*i+5,2*i+6)]))
+    }
+  }
+  if(!(is.null(gwas_p))){
+    gwas_id <- as.vector(gwas_id)
+    gwas_id <- gwas_id[as.vector(gwas_p) <= pvalue]
+    genotype <- matrix(nrow=nrow(ped),ncol=length(c(gwas_id)))
+    rownames(genotype) <- id.select
+    colnames(genotype) <- gwas_id
+    snps<-which(tped[,2]%in%gwas_id)
+    ids<-which(ped[,2]%in%id.select)
+    tped_trim <- tped[snps,c(1:4,sort(rep(2*ids,each=2)-(1:(2*length(ids)))%%2)+4)]
+    ped_trim <- ped[ids,c(1:6,sort(rep(2*snps,each=2)-(1:(2*length(snps)))%%2)+6)]
+    for (i in 1:(dim(genotype)[2])){
+      genotype[,i] <- rowMeans((ped_trim[,c(2*i+5,2*i+6)]))
+    }
+  }
+  return(genotype)
 }
-
 #' Calculate the epistatic interaction effect between SNP pairs to construct a 
-#' WISH network
+#' WISH network using a genotype data frame created from genarate.genotype()
 #' @export
 #' @description A WISH network can be build based on epistatic interaction 
 #' effects between SNP pairs. Those interaction effects are calculated using
 #' ASReml and can be used directly in the WISH network construction.
-#' @usage epistatic.correlation(gwas, pvalue, phenotype, genotype, parallel)
-#' ASReml-R: library("asreml")
-#' package doParralel: library("doParallel")
-#' package sna: library("sna")
-#' @import asreml
+#' @usage epistatic.correlation(phenotype, genotype, parallel)
 #' @import doParallel
-#' @import sna
-#' @param gwas_id A vector of all SNPs in the GWAS
-#' @param gwas_p A vector of the p-values corresponding to the gwas_id vector
-#' @param pvalue A value for the cutoff of the SNPs which should be remained 
-#' in the matrix, based on the pvalue resulting from the GWAS
-#' @param phenotype Dataframe with on the rows the individuals in the analysis,
-#' and columns for the different measured phenotypes and fixed/random factors
-#' (e.g. sex)
+#' @param phenotype Dataframe with the rows correspinding to the individuals
+#' in the analysis,and columns for the different measured phenotypes and 
+#' fixed/random factors. Only give one phenotype column at a time. Phenotypes
+#' should be continous variables. Make sure that the dataframe contains the same
+#' individuals as in the genotype-file, and that those are in the same order.
 #' @param genotype Dataframe with the genotype information, resulting from 
-#' the function data.import. Make sure that the dataframe contains the same
-#' individuals as in the phenotype-file, and that those are in the same order.
+#' the function generate.genotype(). Make sure that the dataframe contains the 
+#' same individuals as in the phenotype-file, and that those are in the 
+#' same order.
 #' @param parallel Number of cores to use for parallel execution in the function 
 #' registerDoParallel()
 #' @return The resulting matrix gives the epistatic interaction effects between
@@ -109,27 +110,32 @@ data.import <- function(ped, tped,gwas_id,gwas_p,pvalue=0.05,id.select=ped[,2]){
 #' epistatic.correlation(gwas, pvalue, phenotype, genotype, parallel)
 #' 
 #' 
-epistatic.correlation <- function(gwas_id,gwas_p,pvalue=10E-5,phenotype,genotype,parallel=20 ){
+epistatic.correlation <- function(phenotype,genotype,parallel=1 ){
   registerDoParallel(parallel)
-  gwas <- cbind(gwas_id,gwas_p)
-  gwas <- as.data.frame(gwas)
-  gwas$gwas_p <- as.numeric(as.character(gwas$gwas_p))
-  gwas <- gwas[gwas$gwas_id%in%colnames(genotype),]
-  cand <- gwas[gwas$gwas_p < pvalue,]
-  snp_matrix <- matrix(NA, nrow=nrow(cand),ncol=nrow(cand))
-  rownames(snp_matrix)<- cand$gwas_id
-  colnames(snp_matrix)<- cand$gwas_id
-  cand <- which(gwas$gwas_p<= pvalue)  
+  snp_matrix <- matrix(NA, nrow=ncol(genotype),ncol=ncol(genotype))
+  rownames(snp_matrix)<- colnames(genotype)
+  colnames(snp_matrix)<- colnames(genotype)
+  if(is.data.frame(genotype)){
+    genotypen <- genotype
+    genotypen[] <- lapply(genotypen, as.numeric)
+    genotypen<-as.matrix(genotypen)
+  }
+  else {
+    genotypen <- genotype
+  }
+  genotype <- as.data.frame(genotype)
+  genotype[] <- lapply(genotype, as.factor)
+  print(dim(snp_matrix))
+  print(dim(genotype))
+  print(dim(genotypen))
   for (i in 1:(nrow(snp_matrix)-1)) {
-    snp_matrix[i, (i+1):nrow(snp_matrix)] <- foreach(j = (i+1):nrow(snp_matrix), .combine='rbind', .inorder=T, .verbose=F) %dopar% { 
-      phenotype$SNP1=as.numeric((genotype[,cand[i]]));
-      phenotype$SNP2=as.numeric((genotype[,cand[j]]));
-      tmp_correlation = asreml(OI~1+SNP1+SNP2+SNP1*SNP2,na.method.X="omit", na.method.Y="omit",data=phenotype) ## <- this function needs to be adjusted according to your trait: trait=trait of interest, fixed and random effects can be added (e.g. sex)
-      return(tmp_correlation$coefficients$fixed[1])
-    } 
+    snp_matrix[i, (i+1):nrow(snp_matrix)] <- foreach(j = (i+1):nrow(snp_matrix), .combine='rbind', .inorder=T, .verbose=F) %dopar% {
+      tmp_correlation = fastLm(phenotype[,1] ~ genotype[,i]+genotype[,j]+I(genotypen[,i]*genotypen[,j]))
+      return(tmp_correlation$coefficients[length(tmp_correlation$coefficients)])
+    }
   }
   new <- t(snp_matrix)
-  diag(new) <- 1 
+  diag(new) <- 1
   new[upper.tri(new)]<- snp_matrix[upper.tri(snp_matrix)]
   return(new)
 }  
