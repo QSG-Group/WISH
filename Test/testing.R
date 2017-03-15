c10bfaef7ea9045617b5cf0518b49b0194a3c984
AQS-Group <vaoc@sund.ku.dk>
2017-03-15 16:01:46 +0100

    testing

--- Test/testing.R
+++ Test/testing.R
@@ -158,7 +158,7 @@ epistatic.correlation_test <- function(phenotype,genotype,parallel=1,test=T,simp
 }
 result1<-epistatic.correlation_test(genotype = genotype[1:193,1:1000], phenotype = phenotype$W0_W12 ,parallel = 10, test = T)
 
-result1<-epistatic.correlation(genotype = genotype[1:193,1:1000], phenotype = phenotype$W0_W12 ,parallel = 10, test = T, simple = F)
+result1<-epistatic.correlation(genotype = genotype[1:193,1:1000], phenotype = phenotype$W0_W12 ,threads = 10, test = F, simple = T)
 hist(result1$Pvalues)
 result1$Coefficients
 
@@ -169,6 +169,132 @@ hist(result$Pvalues)
 ids_delete <- read.table("/home/victor/Documents/WISH_files/IDs_delete.txt")
 phenotype <- read.table("/home/victor/Documents/ADD_files/pheno_for_victor.txt",header = T)
 genotype<-generate.genotype(ped = "/home/victor/Documents/ADD_files/INDICES_1_2.ped",tped = "/home/victor/Documents/ADD_files/INDICES1_1_2.tped")
-tped <- fread("/home/victor/Documents/ADD_files/INDICES1_1_2.tped", data.table = F)
 genotype<-genotype[!(rownames(genotype) %in% ids_delete[,1]),]
+tped <- fread("/home/victor/Documents/ADD_files/INDICES1_1_2.tped", data.table = F)
 
+
+
+
+#### Developing Blocks ####
+
+correlation_blocks <-function(genotype,threshold=0.9){
+  snp_block_matrix <- as.matrix(rep(0,dim(genotype)[2]))
+  rownames(snp_block_matrix) <- colnames(genotype)
+  n_snp <- 1
+  start <- 2
+  n_block <- 1
+  snps <- dim(genotype)[2]
+  snp_block_matrix[n_snp,] <- n_block
+  while(start <= snps){
+    if (n_snp == snps){
+      snp_block_matrix[snps,1] = n_block
+    }
+    else{
+      matches<-sum(genotype[,n_snp]/genotype[,start] == 1,na.rm = T)+sum((genotype[,n_snp]+genotype[,start]) == 3,na.rm = T)+sum(c(c(genotype[,1] == 1.5)+ c(genotype[,3] == 1.5)) == 1 ,na.rm = T)/2
+      total <- sum(!is.na(genotype[,n_snp]+genotype[,start]))
+      similarity <- matches/total
+      if (similarity >= threshold){
+        snp_block_matrix[start,1] = n_block
+        start <- start+1
+      }
+      else {
+        n_snp <- start
+        n_block <- n_block +1
+        snp_block_matrix[start,1] = n_block
+        start <- start +1
+      }
+    }
+  }
+  return(snp_block_matrix)
+} 
+
+blocks<-correlation_blocks(genotype,0.9)
+tail(blocks)
+woot<-sort((table(blocks)),decreasing = T)
+woot[1:100]
+
+correlation_blocks_running <-function(genotype,threshold=0.9){
+  snp_block_matrix <- as.matrix(rep(0,dim(genotype)[2]))
+  rownames(snp_block_matrix) <- colnames(genotype)
+  n_snp <- 1
+  start <- 2
+  n_block <- 1
+  snps <- dim(genotype)[2]
+  snp_block_matrix[n_snp,] <- n_block
+  similarity <- 0
+  while(start <= snps){
+    if (n_snp == snps){
+      snp_block_matrix[snps,1] = n_block
+    }
+    else{
+      matches<-sum(genotype[,n_snp]/genotype[,start] == 1,na.rm = T)+sum((genotype[,n_snp]+genotype[,start]) == 3,na.rm = T)+sum((genotype[,n_snp] == 1.5 + genotype[,start] == 1.5)==1,na.rm = T)
+      total <- sum(!is.na(genotype[,n_snp]+genotype[,start]))
+      score<-matches+(total-matches)/2
+      similarity <- score/total
+      if (similarity/(start-n_snp) >= threshold){
+        snp_block_matrix[start,1] = n_block
+        start <- start+1
+      }
+      else {
+        n_snp <- start
+        n_block <- n_block +1
+        snp_block_matrix[start,1] = n_block
+        start <- start +1
+        similarity <- 0
+      }
+    }
+  }
+  return(snp_block_matrix)
+} 
+
+
+blocks<-correlation_blocks_running(genotype)
+tail(blocks)
+
+
+correlation_blocks_network <-function(genotype,threshold=0.9){
+  snp_block_matrix <- as.matrix(rep(0,dim(genotype)[2]))
+  rownames(snp_block_matrix) <- colnames(genotype)
+  n_snp <- 1
+  start <- 2
+  n_block <- 1
+  snps <- dim(genotype)[2]
+  snp_block_matrix[n_snp,] <- n_block
+  while(start <= snps){
+    if (n_snp == snps){
+      snp_block_matrix[snps,1] = n_block
+    }
+    else{
+      network_coord <- combn(start:n_snp,2)
+      similarity <- rep(0,dim(network_coord)[2])
+      for (i in 1:dim(network_coord)[2]){
+        matches<-sum(genotype[,network_coord[1,i]]/genotype[,network_coord[2,i]] == 1,na.rm = T)+sum((genotype[,network_coord[1,i]]+genotype[,network_coord[2,i]]) == 3,na.rm = T)+sum(c(c(genotype[,network_coord[1,i]] == 1.5)+ c(genotype[,network_coord[2,i]] == 1.5)) == 1 ,na.rm = T)/2
+        total <- sum(!is.na(genotype[,network_coord[1,i]]+genotype[,network_coord[2,i]]))
+        similarity[i] <- matches/total
+      }
+      if (sum(similarity)/(dim(network_coord)[2]) >= threshold){
+        snp_block_matrix[start,1] = n_block
+        start <- start+1
+      }
+      else {
+        n_snp <- start
+        n_block <- n_block +1
+        print(c(start,n_block,dim(network_coord)[2]))
+        snp_block_matrix[start,1] = n_block
+        start <- start +1
+        similarity <- 0
+      }
+    }
+  }
+  return(snp_block_matrix)
+} 
+
+blocks<-correlation_blocks_network(genotype,0.95)
+tail(blocks)
+
+matches<-sum(tempgen[,1]/tempgen[,2] == 1,na.rm = T)+sum((tempgen[,1]+tempgen[,2]) == 3,na.rm = T)
+score<-matches+(sum(!is.na(tempgen[,1]+tempgen[,2]))-matches)/2
+sum(c(c(genotype[,1] == 1.5)+ c(genotype[,3] == 1.5)) == 1 ,na.rm = T)
+
+sum((genotype[,1]+genotype[,3]) == 2.5)
+sum((genotype[,1]+genotype[,3]) == 3.5)
